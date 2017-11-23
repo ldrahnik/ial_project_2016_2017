fd7f8de4a0a159a7ae0134e1c647dbd854160c67
Lukáš Drahník <ldrahnik@gmail.com>
2017-11-23 12:53:28 +0100

    typo testing

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,7 +1,13 @@
 #usr/bin/sh
 
-# test01
+# test01 - non oriented non rated
 ./shreya -i ./tests/test01
+
+# test01 - oriented non rated
 #./shreya -i ./tests/test01 -o
+
+# test01 - non oriented rated
 #./shreya -i ./tests/test01 -r
+
+# test01 - oriented rated
 #./shreya -i ./tests/test01 -o -r
